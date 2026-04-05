2ae8a52c33c4eea0a3867235412689cec889f97c
MD ZINNURAIN <98215729+zinnurain250@users.noreply.github.com>
2026-04-06 00:09:25 +0600

    Update main.tsx

--- artifacts/inventory-dashboard/src/main.tsx
+++ artifacts/inventory-dashboard/src/main.tsx
@@ -1,5 +1,9 @@
 import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./index.css";
+import { setBaseUrl } from "@workspace/api-client-react";
+
+// সরাসরি আপনার ব্যাকএন্ড লিংকটি এখানে বসিয়ে দিন
+setBaseUrl("https://inventory-nexus-api-server-75vc.vercel.app");
 
 createRoot(document.getElementById("root")!).render(<App />);
